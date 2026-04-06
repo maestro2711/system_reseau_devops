95893b41470939da1818ab368e616fa4424106ce
assagui <asmitterand@yahoo.fr>
2026-04-06 16:52:37 +0200

    ovveride log

--- manage_lvm.sh
+++ manage_lvm.sh
@@ -7,30 +7,30 @@ manage_lvm() {
     CURRENT=${USAGE[$path]}
     CURRENT_CAUSE=${CAUSE[$path]}
 
-    LOG_FILE="/var/log/monitor.log"
+    LOG_FILE="/tmp/monitor.log"
 
-    # 🔹 seuil critique
+    #  seuil critique
     if [ "$CURRENT" -lt 95 ]; then
         return
     fi
 
-    # 🔹 sécurité (ne pas masquer bug/attaque)
+    #  sécurité (ne pas masquer bug/attaque)
     if [ "$CURRENT_CAUSE" = "BUG" ] || [ "$CURRENT_CAUSE" = "ATTACK" ]; then
         echo "$(date) - REFUSED LVM ($CURRENT_CAUSE) on $path" >> "$LOG_FILE"
         return
     fi
 
-    # 🔹 récupérer le device (LV)
+    #  récupérer le device (LV)ssss
     LV_PATH=$(df -h "$path" | awk 'NR==2 {print $1}')
 
-    # 🔹 type filesystem
+    #  type filesystem
     FS_TYPE=$(df -T "$path" | awk 'NR==2 {print $2}')
 
-    # 🔹 récupérer VG
+    #  récupérer VG
     VG_NAME=$(lvs --noheadings -o vg_name "$LV_PATH" | tr -d ' ')
     VG_FREE=$(vgs --noheadings -o vg_free --units g "$VG_NAME" | tr -d ' g')
 
-    # 🔹 cas 1 : espace disponible dans VG
+    #  cas 1 : espace disponible dans VG
     if (( $(echo "$VG_FREE > 1" | bc -l) )); then
 
         lvextend -L +1G "$LV_PATH"
@@ -45,7 +45,7 @@ manage_lvm() {
         return
     fi
 
-    # 🔥 cas 2 : VG plein → chercher nouveau disque sécurisé
+    #  cas 2 : VG plein → chercher nouveau disque sécurisé
 
     for disk in $(lsblk -dn -o NAME,TYPE | grep disk | awk '{print $1}'); do
 
